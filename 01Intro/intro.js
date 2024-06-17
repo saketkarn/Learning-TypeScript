e3bfffc5a5369aff894f990dc3b879118e3e3d17
Saket Karn <saketkarn38@gmail.com>
2024-06-17 15:15:04 +0530

    intro

---
+++ 01Intro/intro.js
@@ -0,0 +1,6 @@
+var user = {
+    name: "Saket",
+    age: 25
+};
+console.log("Saket");
+console.log(user.name);
---
+++ 01Intro/intro.ts
@@ -0,0 +1,6 @@
+let user={
+    name:"Saket",
+    age:25}
+    
+console.log("Saket")
+console.log(user.name)
