344cd7a9f74ff28861a47031c846b32234c8a3f0
Saket Karn <saketkarn38@gmail.com>
2024-06-17 17:48:45 +0530

    Objects

---
+++ 02Basics/myObjects.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var User = {
+    name: "Saket",
+    email: "saket@gmail.com",
+    isActive: true
+};
+function createUser(_a) {
+    var string = _a.name, boolean = _a.isPaid;
+}
+createUser({ name: "Saket", isPaid: false });
+function createCourse() {
+    return { name: "Saket", price: 369 };
+}
---
+++ 02Basics/myObjects.ts
@@ -0,0 +1,18 @@
+const User={
+    name:"Saket",
+    email:"saket@gmail.com",
+    isActive:true
+}
+
+function createUser({name:string, isPaid:boolean}){
+
+}
+
+createUser({name:"Saket",isPaid:false})
+
+
+function createCourse():{name:string,price:number}{
+    return {name:"Saket",price:369}
+}
+
+export{}
