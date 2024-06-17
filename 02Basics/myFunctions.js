9a0268b481f2aaa65b70ce5d1afad04e26d92be5
Saket Karn <saketkarn38@gmail.com>
2024-06-17 17:39:48 +0530

    functions

--- 02Basics/myFunctions.js
+++ 02Basics/myFunctions.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 function addTwo(num) {
     return num + 2;
+    // return "hello"  this won't be allowed
 }
 addTwo(5);
 function getUpper(val) {
@@ -15,3 +16,16 @@ var loginUser = function (name, email, isPaid) {
     if (isPaid === void 0) { isPaid = false; }
 };
 loginUser("saket", "s@s.com");
+var getHello = function (s) {
+    return "";
+};
+var heros = ["thor", "spiderman", "ironman"];
+heros.map(function (hero) {
+    return "hero is ".concat(hero);
+});
+var consoleError = function (errmsg) {
+    console.log(errmsg);
+};
+var handleError = function (errmsg) {
+    throw new Error(errmsg);
+};
--- 02Basics/myFunctions.ts
+++ 02Basics/myFunctions.ts
@@ -1,5 +1,6 @@
-function addTwo(num:number){
+function addTwo(num:number):number{  //now function would only return a number and not any other type of value
     return num+2
+    // return "hello"  this won't be allowed
 }
 
 addTwo(5) 
@@ -22,3 +23,21 @@ let loginUser=(name:string, email:string, isPaid:boolean=false)=>{ //giving the 
 }
 
 loginUser("saket","s@s.com")
+
+const getHello=(s:string):string=>{  //arrow function returning only string values
+    return ""
+}
+
+const heros=["thor","spiderman","ironman"]
+
+heros.map((hero):string=>{
+    return `hero is ${hero}`
+})
+
+let consoleError=(errmsg:string):void=>{  //this function won't return anything
+    console.log(errmsg)
+}
+
+let handleError=(errmsg:string):never=>{  //it'd never return any value, it's used to handle errors
+    throw new Error(errmsg)
+}
