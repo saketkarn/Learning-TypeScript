2a9e0aac8a3d0796975debde3d2159fea8e2cbde
Saket Karn <saketkarn38@gmail.com>
2024-06-17 17:07:43 +0530

    type inference

--- 01Intro/variableMe.js
+++ 01Intro/variableMe.js
@@ -1,2 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var greetings = "Hello Saket";
+greetings.toLowerCase();
 console.log(greetings);
+//number
+var userId = 9878;
+//boolean
+var isLoggedIn = false;
+//type inference
+var rollNo = 69;
+rollNo.toFixed(); //here we haven't defined any datatype with rollNo but still TS takes it as a number as it's smart enough for this.
--- 01Intro/variableMe.ts
+++ 01Intro/variableMe.ts
@@ -2,4 +2,18 @@ let greetings:string="Hello Saket"
 greetings.toLowerCase()
 console.log(greetings)
 
+
+//number
+let userId : number = 9878
+
+//boolean
+let isLoggedIn : boolean = false
+
+
+//type inference
+
+let rollNo=69
+rollNo.toFixed()  //here we haven't defined any datatype with rollNo but still TS takes it as a number as it's smart enough for this.
+//So, we don't always need to put the datatype along with the variables we've defined.
+
 export{}
