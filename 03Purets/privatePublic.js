ed5bd0e35f6844b103db57f9c394afe6b7d15811
Saket Karn <saketkarn38@gmail.com>
2024-06-19 20:41:41 +0530

    gettersSetters

--- 03Purets/privatePublic.js
+++ 03Purets/privatePublic.js
@@ -1,11 +1,47 @@
+// class newUser{
+//     public email:string
+//     name:string
+//     private readonly city:string="Patna"
+//     constructor(email:string, name:string){
+//         this.email=email
+//         this.name=name
+//     }
+// }
 var newUser = /** @class */ (function () {
     function newUser(email, name) {
-        this.city = "Patna";
         this.email = email;
         this.name = name;
+        this._courseCount = 1;
+        this.city = "Patna";
     }
+    newUser.prototype.deleteToken = function () {
+        console.log("Token deleted");
+    };
+    Object.defineProperty(newUser.prototype, "getAppleEmail", {
+        get: function () {
+            return "apple ".concat(this.email);
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(newUser.prototype, "courseCount", {
+        get: function () {
+            return this._courseCount;
+        },
+        set: function (courseNum) {
+            if (courseNum <= 1) {
+                throw new Error("course count must be greater than 1");
+            }
+            else {
+                this.courseCount = courseNum;
+            }
+        },
+        enumerable: false,
+        configurable: true
+    });
     return newUser;
 }());
 var karn = new newUser("saket", "sak@sa.com");
 // karn.city //currently accessible before declaring the city as private
 // karn.city   //after declaring it as private it won't no longer be accessible
+// karn.deleteToken() //can't be accessed as it's a private function
--- 03Purets/privatePublic.ts
+++ 03Purets/privatePublic.ts
@@ -1,13 +1,50 @@
+// class newUser{
+//     public email:string
+//     name:string
+//     private readonly city:string="Patna"
+//     constructor(email:string, name:string){
+//         this.email=email
+//         this.name=name
+//     }
+// }
+
 class newUser{
-    public email:string
-    name:string
-    private readonly city:string="Patna"
-    constructor(email:string, name:string){
-        this.email=email
-        this.name=name
+
+    private _courseCount=1
+
+    readonly city:string="Patna"
+    constructor(
+        public email:string,
+        public name:string,
+        // private userId:string
+    ){
     }
+
+    private deleteToken(){
+        console.log("Token deleted")
+    }
+
+    get getAppleEmail():string{
+        return `apple ${this.email}`
+    }
+
+    get courseCount():number{
+        return this._courseCount
+    }
+
+    set courseCount(courseNum){
+        if(courseNum<=1){
+            throw new Error("course count must be greater than 1")
+        }
+        else{
+            this.courseCount=courseNum
+        }
+    }
+
 }
 
 const karn = new newUser("saket","sak@sa.com")
 // karn.city //currently accessible before declaring the city as private
 // karn.city   //after declaring it as private it won't no longer be accessible
+
+// karn.deleteToken() //can't be accessed as it's a private function
