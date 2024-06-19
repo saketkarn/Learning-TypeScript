5e42ac137ef5d3e322c293807d15b7652a518a53
Saket Karn <saketkarn38@gmail.com>
2024-06-19 15:36:49 +0530

    tuple

---
+++ 02Basics/myTupEnums.js
@@ -0,0 +1,6 @@
+// const user:(string|number)[]=[1,"saket"]
+//here the order doesn't matter, we can store the values in any order.
+//but in some cases, like storing of the data fetched from api calls, the order of execution is very important..
+var tUser = ["saket", 123, true];
+var rgb = [255, 123, 112];
+var tuNewUser = [123, "saket"];
---
+++ 02Basics/myTupEnums.ts
@@ -0,0 +1,13 @@
+// const user:(string|number)[]=[1,"saket"]
+//here the order doesn't matter, we can store the values in any order.
+
+//but in some cases, like storing of the data fetched from api calls, the order of execution is very important..
+
+
+let tUser:[string, number, boolean]=["saket",123,true]
+
+let rgb:[number, number, number]=[255,123,112]
+
+type tuUser=[number, string]
+
+const tuNewUser:tuUser=[123,"saket"]
